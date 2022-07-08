cbebd689b7dd5c244d783cd54be57ef94327847e
SpicyClown69 <101468777+SpicyClown69@users.noreply.github.com>
2022-07-08 17:29:05 +0100

    initial commit ig

---
+++ changeicon.js
@@ -0,0 +1,2 @@
+document.querySelector('link[rel*="icon"]').href = chrome.runtime.getURL("DarkBearingSmall.ico")
+document.querySelector('link[rel="icon"]').href = chrome.runtime.getURL("DarkBearingSmall.ico")
